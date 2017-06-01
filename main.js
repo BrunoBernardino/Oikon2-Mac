d21331c0e16418780ec398aaf56cd22c430202f4
Bruno Bernardino <bruno@bernardino.io>
2017-06-01 18:03:49 +0100

    Fix guidelines issue

--- main.js
+++ main.js
@@ -48,9 +48,7 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on('activate', () => {
